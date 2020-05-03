da7471f2552bd63636ca17deb3471e5bfe370e96
Aine <m.g.firefox@gmail.com>
2020-05-03 18:49:26 -0400

    tried to make breaks work and broke stuff, oop

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,38 @@
+from random import random
+
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from datetime import datetime
 from werkzeug.urls import url_parse
-from app.forms import LoginForm, RegistrationForm
-from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home')
+
 
 @app.route('/goToHomePage', methods=['GET', 'POST'])
 def goToHomePage():
     return render_template('index.html', title='Home')
 
+
 @app.route('/goToSciencePage', methods=['GET', 'POST'])
 def goToSciencePage():
     return render_template('science.html', title='Science')
 
+
+interests = []
+
+
 @app.route('/goToSetupPage', methods=['GET', 'POST'])
 def goToSetupPage(form):
-    interests= []
     for x in range(4):
         name= "checkbox"+str(x+1)
         if form.name.checked:
             interests.append(form.name.value)
     return render_template('setup.html', title='Setup')
+
 
 @app.route('/goToWorkPage', methods=['GET', 'POST'])
 def goToWorkPage():
@@ -38,10 +45,41 @@ def goToWorkPage():
 
 @app.route('/goToBreakPage', methods=['GET', 'POST'])
 def goToBreakPage():
-    return render_template('break.html', title='Break')
+    loadBreaks()
+    current = chooseBreak()
+    return render_template('break.html', title='Break', content=current.content)
+
 
 class Break:
     def __init__(fun, type, content):
         fun.type = type
         fun.content = content
 
+
+breaks = []
+
+
+def loadBreaks():
+    thing1 = Break("music", "https://www.youtube.com/watch?v=1YAf8hFX0M0")
+    breaks.append(thing1)
+
+
+def chooseBreak():
+    if len(interests) == 0:
+        interests.append("music")
+    if len(interests) > 1:
+        interest = getRandNum(len(interests))
+    else:
+        interest = 0
+    strInterest= interests[interest]
+    possible= []
+    for x in breaks:
+        if breaks[x].type == strInterest:
+            possible.append(breaks[x])
+
+    theBreak= possible[getRandNum(len(possible))]
+    return theBreak
+
+
+def getRandNum(range):
+    return random.randrange(0, range)

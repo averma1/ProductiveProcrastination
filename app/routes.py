74ff84d02b534b730450cc563d2c053223026b0c
Aine <m.g.firefox@gmail.com>
2020-05-03 11:02:01 -0400

    break class and clean up

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, current_user, login_required
 from datetime import datetime
 from werkzeug.urls import url_parse
 from app.forms import LoginForm, RegistrationForm
@@ -11,56 +10,6 @@ from app.models import User
 def index():
 	return render_template('index.html', title='Home')
 
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm(request.form)
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm(request.form)
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('index'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/goToAccountPage', methods=['GET', 'POST'])
-def goToAccountPage():
-    return render_template('account.html', title='Account')
-
 @app.route('/goToHomePage', methods=['GET', 'POST'])
 def goToHomePage():
     return render_template('index.html', title='Home')
@@ -69,9 +18,13 @@ def goToHomePage():
 def goToSciencePage():
     return render_template('science.html', title='Science')
 
-
 @app.route('/goToSetupPage', methods=['GET', 'POST'])
-def goToSetupPage():
+def goToSetupPage(form):
+    interests= []
+    for x in range(4):
+        name= "checkbox"+str(x+1)
+        if form.name.checked:
+            interests.append(form.name.value)
     return render_template('setup.html', title='Setup')
 
 @app.route('/goToWorkPage', methods=['GET', 'POST'])
@@ -86,3 +39,9 @@ def goToWorkPage():
 @app.route('/goToBreakPage', methods=['GET', 'POST'])
 def goToBreakPage():
     return render_template('break.html', title='Break')
+
+class Break:
+    def __init__(fun, type, content):
+        fun.type = type
+        fun.content = content
+
